695709eb2c332f46a7de4a46e288f6c1027c77eb
Brenda Joshua <brendajosh.bj@gmail.com>
2019-10-09 17:41:53 +0300

    created new action creators

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import React from 'react';
-
+import { addFeature, removeFeature } from './actions/actionTypes';
+import { connect } from 'react-redux';
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
+const App = (props) => {
+
   const removeFeature = item => {
     // dispatch an action here to remove an item
+    props.removeFeature(item);
   };
 
-  const buyItem = item => {
+  const addFeature = item => {
     // dipsatch an action here to add an item
+    props.addFeature(item);
   };
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures store={props.store} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    store: state.store
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  { addFeature, removeFeature }
+)(App);
